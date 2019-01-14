3442a3197995bb1a485966bc4d487dfe68231843
Tess <tess.thornley@gmail.com>
2019-01-14 18:08:43 +0000

    Adds three loop exercises

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+    i ++;
+}
+
+for (var i = 0; i < ingredients.length; i ++) {
+  console.log(ingredients[i]);
+}
+
+for (var i = 7; i >= 0; i --) {
+  console.log(ingredients[i]);
+}
